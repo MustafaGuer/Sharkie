fe45ab5065cd39442fc8e1501e5f1fdb5dce04e1
MustafaGuer <mustafaguer@outlook.com>
2021-06-28 07:50:07 +0200

    added font and bg image

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-let canvas;
-let ctx;
+let canvas; // Die "Leinwand" auf der gezeichnet wird
+let ctx;    // Mit dem Context zeichnet man auf den Canvas (der Pinsel im übertragenen Sinn)
 
 function init() {
     
@@ -7,7 +7,7 @@ function init() {
     canvas = document.getElementById('canvas');
 
     ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = 'white';  // .fillStyle bewirkt das der ctx mit einer farbe gefüllt wird
+    ctx.fillRect(0, 0, canvas.width, canvas.height); // mit .fillRect sagt man das man ein rechteck malen will mit den Werten in den Klammer
 
 }
