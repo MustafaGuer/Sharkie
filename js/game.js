0349a12ec09d9a8e58ba9d08a9669d5db5f1c144
MustafaGuer <mustafaguer@outlook.com>
2021-06-28 08:16:04 +0200

    added first img

--- js/game.js
+++ js/game.js
@@ -1,10 +1,18 @@
 let canvas; // Die "Leinwand" auf der gezeichnet wird
 let ctx;    // Mit dem Context zeichnet man auf den Canvas (der Pinsel im übertragenen Sinn)
+let character = new Image();
+
 
 function init() {
-    
-    
-    canvas = document.getElementById('canvas');
 
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+
+
+
+    character.src = '../img/1.Sharkie/3.Swim/1.png';
+
+    ctx.drawImage(character, 20, 20, 50, 150);
 
 }
