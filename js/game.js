eb5cba5baca3a5ddf4e2c95932b7679f0ac77868
MustafaGuer <mustafaguer@outlook.com>
2021-06-28 08:02:51 +0200

    added favicon

--- js/game.js
+++ js/game.js
@@ -6,8 +6,5 @@ function init() {
     
     canvas = document.getElementById('canvas');
 
-    ctx = canvas.getContext('2d');
-    ctx.fillStyle = 'white';  // .fillStyle bewirkt das der ctx mit einer farbe gefüllt wird
-    ctx.fillRect(0, 0, canvas.width, canvas.height); // mit .fillRect sagt man das man ein rechteck malen will mit den Werten in den Klammer
 
 }
